6b49cf0145a9434cc985c7c4e62dbf472642228b
akunplinplanman <akunplinplanman@gmail.com>
2025-03-04 19:58:24 +0700

    Tipe Data

---
+++ dart_project/bin/Data Types/Num.dart
@@ -0,0 +1,7 @@
+void main() {
+  num angka = 100;
+  print(angka);
+
+  num angka2 = 10;
+  print(angka2);
+}
---
+++ dart_project/bin/Data Types/String.dart
@@ -0,0 +1,4 @@
+void main() {
+  String nama = 'Azhar ';
+  print('Nama Saya ' + nama);
+}
